c4a8b4f2d53b9228dceed324d698a8b6c04d002e
feninpranjal <138654067+feninpranjal@users.noreply.github.com>
2025-04-02 03:10:43 +1100

    Initial commit for strata-management

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Welcome to Strata Management</h1>
+      <p>This website helps manage apartment buildings efficiently.</p>
+    </main>
+  );
+}
